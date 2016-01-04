ede5da4428e27523bf0422647040eedce36f409a
jack101101 <979900351@qq.com>
2016-01-04 14:35:09 +0800

    Initial Commit

---
+++ MobilePhoneSchool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobilePhoneSchool
+//
+//  Created by 李策 on 16/1/4.
+//  Copyright © 2016年 mobilephoneschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobilePhoneSchool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MobilePhoneSchool
+//
+//  Created by 李策 on 16/1/4.
+//  Copyright © 2016年 mobilephoneschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
